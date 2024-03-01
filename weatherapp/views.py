c57e64d6228e4b8d9a9217c151083e50112c602e
Stan1-0 <stankofb@gmail.com>
2024-03-01 16:45:41 +0000

    Update weather app to display more weather information from the api.

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        location = request.POST['location']
+        res =  urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+location+'6a0a8e5b129c6238ebf3195d7927099f').read()
+        json_data = json.loads(res) 
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "temperature": float(json_data['main']['temp'])+'k',
+            "description": json_data['weather'][0]['description'],
+            "coordinates": str(json_data['coord']['lon'])+ '' + str(json_data['cord']['lat']),
+            "pressure": str(json_data['main']['pressure']),
+            'humidty': str(json_data['main']['humidity']),
+            
+            }
+    else:
+        data = {}
+    return render(request, 'index.html', data )
