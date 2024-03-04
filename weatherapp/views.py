bc5681f5a3ad5bf02b3b3d6ac0c2b26645d3795b
Stan1-0 <stankofb@gmail.com>
2024-03-04 22:15:10 +0000

    Update weather API endpoint in views.py

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -6,13 +6,13 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         location = request.POST['location']
-        res =  urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast?q='+location+'6a0a8e5b129c6238ebf3195d7927099f').read()
+        res =  urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=6a0a8e5b129c6238ebf3195d7927099f').read()
         json_data = json.loads(res) 
         data = {
             "country_code": str(json_data['sys']['country']),
-            "temperature": float(json_data['main']['temp'])+'k',
+            "temperature": str(float(json_data['main']['temp']))+'k',
             "description": json_data['weather'][0]['description'],
-            "coordinates": str(json_data['coord']['lon'])+ '' + str(json_data['cord']['lat']),
+            "coordinates": str(json_data['coord']['lon'])+ '' + str(json_data['coord']['lat']),
             "pressure": str(json_data['main']['pressure']),
             'humidty': str(json_data['main']['humidity']),
             
