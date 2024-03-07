c2c2f8a1946ad49720191b18feb9830960341af8
Stan1-0 <stankofb@gmail.com>
2024-03-07 12:56:32 +0000

    generated requirements for weatherapp

--- weather/settings.py
+++ weather/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-of)kaaxeki%)&-j0&vzsg4)qt&7%&)-76*z*@=kxh=hp@gk12!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
